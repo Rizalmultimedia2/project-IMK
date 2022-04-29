44c08e1027f7bfa9a589940686b0912a2cb81bf0
Rian Febriansyah <rianfebriansyah22@gmail.com>
2022-04-29 14:48:19 +0700

    add character rootation

---
+++ Assets/Scripts/CharacterRotation.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class CharacterRotation : MonoBehaviour
+{
+    public Transform Player;
+
+    void Update()
+    {
+        Player.transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+    }
+}
