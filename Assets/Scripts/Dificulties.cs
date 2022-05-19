e7d5470b32005e3d3f03d191d3b003025f9158f1
Rian Febriansyah <rianfebriansyah22@gmail.com>
2022-05-19 11:03:04 +0700

    add dificulties for island count

--- Assets/Scripts/Dificulties.cs
+++ Assets/Scripts/Dificulties.cs
@@ -8,15 +8,18 @@ public class Dificulties : MonoBehaviour
     {
         SceneManager.LoadScene(mainScene);
         PlayerPrefs.SetFloat("bearSpeed", 1f);
+        PlayerPrefs.SetInt("islandsCount", 2);
     }
     public void Medium()
     {
         SceneManager.LoadScene(mainScene);
         PlayerPrefs.SetFloat("bearSpeed", 1.5f);
+        PlayerPrefs.SetInt("islandsCount", 3);
     }
     public void Hard()
     {
         SceneManager.LoadScene(mainScene);
         PlayerPrefs.SetFloat("bearSpeed", 2.5f);
+        PlayerPrefs.SetInt("islandsCount", 4);
     }
 }
